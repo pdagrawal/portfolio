4f01fda10071170f04c1bca535656b2d3918e72c
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2023-05-01 11:01:21 -0400

    fixed the exists issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def svg(name)
     file_path = "#{Rails.root}/app/assets/images/#{name}.svg"
-    if File.exists?(file_path)
+    if File.exist?(file_path)
       Rails.cache.fetch("svg_logo_#{name}") { File.read(file_path).html_safe }
     else
       '(not found)'
