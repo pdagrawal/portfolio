84de65e933e1a9d1c8710525cde34f9133ae6bd0
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2023-05-01 00:55:10 -0400

    Added content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def svg(name)
+    file_path = "#{Rails.root}/app/assets/images/#{name}.svg"
+    if File.exists?(file_path)
+      Rails.cache.fetch("svg_logo_#{name}") { File.read(file_path).html_safe }
+    else
+      '(not found)'
+    end
+  end
 end
